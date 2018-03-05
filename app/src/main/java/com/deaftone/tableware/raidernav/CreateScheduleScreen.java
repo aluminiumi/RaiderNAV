df1875752cd336834c2e5f6b33a3ac9b68624f4d
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2018-03-04 19:17:14 -0600

    fixed codeclimate complaints

--- app/src/main/java/com/deaftone/tableware/raidernav/CreateScheduleScreen.java
+++ app/src/main/java/com/deaftone/tableware/raidernav/CreateScheduleScreen.java
@@ -1,7 +1,7 @@
 package com.deaftone.tableware.raidernav;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,8 +16,9 @@ public class CreateScheduleScreen extends AppCompatActivity {
         button =(Button) findViewById(R.id.addAnotherClass);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onClick(View view) {
+                //TODO: implement
+                System.out.println("Do stuff");
             }
         });
     }
