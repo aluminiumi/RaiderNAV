8f3168592a835ecce80a4bdbd86d9c0ee338ed57
Unknown <sakshyam.silwal@ttu.edu>
2018-03-06 01:28:36 -0600

    in progress

--- app/src/main/java/com/deaftone/tableware/raidernav/CreateScheduleScreen.java
+++ app/src/main/java/com/deaftone/tableware/raidernav/CreateScheduleScreen.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateScheduleScreen extends AppCompatActivity {
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Button finishCreatingSchedule = (Button) findViewById(R.id.finishCreatingSchedule);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_schedule_screen);
 
@@ -20,5 +23,18 @@ public class CreateScheduleScreen extends AppCompatActivity {
 
             }
         });
+       /* finishCreatingSchedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             /*   EditText scheduleName= (EditText) findViewById(R.id.scheduleName);
+              //  String sN= scheduleName.getText().toString();
+                EditText courseName= (EditText) findViewById(R.id.courseName);
+                EditText buildingName= (EditText) findViewById(R.id.buildingName);
+                EditText startTime= (EditText) findViewById(R.id.startTime);
+                EditText endTime= (EditText) findViewById(R.id.endTime);
+              //  courseName.setText(sN);
+
+            }
+        });*/
     }
 }
