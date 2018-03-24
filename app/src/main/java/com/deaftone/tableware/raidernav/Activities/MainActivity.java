f3e24e656c0aeaf2c8291f2f89e1714c4b770c66
Aluminiumi <22565619+aluminiumi@users.noreply.github.com>
2018-03-24 12:58:22 -0500

    staticized addressmap class plus getkeys method

--- app/src/main/java/com/deaftone/tableware/raidernav/Activities/MainActivity.java
+++ app/src/main/java/com/deaftone/tableware/raidernav/Activities/MainActivity.java
@@ -1,15 +1,22 @@
 package com.deaftone.tableware.raidernav.Activities;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.deaftone.tableware.raidernav.R;
+import com.deaftone.tableware.raidernav.ScheduleEntryList;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -21,15 +28,65 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //the button for single destinations
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Not yet implemented!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // get prompt.xml view
+                LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                View promptsView = li.inflate(R.layout.prompt_spinner, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        MainActivity.this);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final TextView tv = (TextView) promptsView.findViewById(R.id.textView1);
+                tv.setText("Where would you like to go?");
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+
+                //userInput.setText();
+                //userInput.setHint("Schedule Name (e.g., \"Spring 2018\")");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(true)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        if (getParent() == null) {
+                                            startActivity(new Intent(MainActivity.this,MapsActivity.class));
+                                            //setResult(Activity.RESULT_OK, getIntent());
+                                        } else {
+                                            //getParent().setResult(Activity.RESULT_OK, getIntent());
+                                        }
+                                        //finish();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                System.out.println("MainActivity: alertDialog created");
+
+                // show it
+                alertDialog.show();
             }
         });
 
+        //the button to view scheduled maps
         FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
         fab2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //the settings button
         FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
         fab3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //the view schedule button
         Button vsb = (Button) findViewById(R.id.viewschedulesbutton);
         vsb.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //the today's classes button
         Button todaysClassesButton = (Button) findViewById(R.id.todaysclassesbutton);
         todaysClassesButton.setOnClickListener(new View.OnClickListener() {
             @Override
