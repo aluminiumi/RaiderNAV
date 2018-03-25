f2d04e857460bdf8ead751a4c3d7b2b7028f6876
Aluminiumi <22565619+aluminiumi@users.noreply.github.com>
2018-03-24 20:38:35 -0500

    full oneshut functionality

--- app/src/main/java/com/deaftone/tableware/raidernav/Activities/MapsActivity.java
+++ app/src/main/java/com/deaftone/tableware/raidernav/Activities/MapsActivity.java
@@ -3,12 +3,16 @@ package com.deaftone.tableware.raidernav.Activities;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.akexorcist.googledirection.DirectionCallback;
 import com.akexorcist.googledirection.GoogleDirection;
@@ -23,15 +27,17 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnMyLocationButtonClickListener, LocationListener, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
     private static final int LOCATION_REQUEST = 500;
@@ -39,88 +45,173 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private static final int INITIAL_STROKE_WIDTH_PX = 10;
 
+    Location myLocation;
     LatLng singleDestination; //gps coordinates for destination
     String singleDestinationName;
+    LocationManager locationManager;
+    String bestProvider;
 
+    @SuppressLint({"MissingPermissions", "MissingPermission"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
         Bundle extras = getIntent().getExtras();
-        singleDestinationName = extras.getString("destinationName");
-        if(singleDestinationName != null) {
-            String destXYstring = AddressMap.fetch(singleDestinationName); //looks like "33.593170, -101.897627"
-            System.out.println(destXYstring);
-            double[] dests = AddressMap.parseXY(destXYstring);
-            singleDestination = new LatLng(dests[0], dests[1]);
+
+        if(havePermissions()) {
+
+            singleDestinationName = extras.getString("destinationName");
+            if(singleDestinationName != null) {
+                String destXYstring = AddressMap.fetch(singleDestinationName); //looks like "33.593170, -101.897627"
+                System.out.println("onCreate: destXYstring: "+destXYstring);
+                double[] dests = AddressMap.parseXY(destXYstring);
+                singleDestination = new LatLng(dests[0], dests[1]);
+            }
+
+
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            listPoints = new ArrayList<>();
+
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            bestProvider = locationManager.getBestProvider(criteria, true);
+            for (String provider : locationManager.getAllProviders()) {
+                locationManager.requestLocationUpdates(provider, 1000, 0, this);
+            }
+            myLocation = getLastKnownLocation();
+            if (myLocation != null) {
+                onLocationChanged(myLocation);
+            }
+
+        }
+    }
+
+    @SuppressLint({"MissingPermissions", "MissingPermission"})
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        System.out.println("ONREQUESTPERMISSIONSRESULT");
+        switch (requestCode) {
+            case LOCATION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mMap.setMyLocationEnabled(true);
+
+                }
+                break;
+        }
+        //super.onRequestPermissionsResult ( requestCode, permissions, grantResults );
+        // TTU memorial circle: 33.584468, -101.874658
+        //LatLng ttu = new LatLng(33.584468, -101.874658);
+        //mMap.addMarker(new MarkerOptions().position(ttu).title("Texas Tech Memorial Circle"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(ttu));
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        if(havePermissions()) {
+            myLocation = getLastKnownLocation();
+            System.out.println("onMyLocationButtonClick(): Last known location: " + myLocation);
+            Toast.makeText(this, "Last known location: "+myLocation, Toast.LENGTH_SHORT).show();
+
+            //onLocationChanged(myLocation);
+            if (myLocation != null) {
+                onLocationChanged(myLocation);
+                if(mMap != null) {
+                    doPathToSingleDestination();
+                }
+            }
+
         }
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        listPoints = new ArrayList<>();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    public void onLocationChanged(Location location) {
+        Toast.makeText(this, "location changed: "+location, Toast.LENGTH_SHORT).show();
+        System.out.println("location changed: "+location);
+        myLocation = location;
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        System.out.println("Provider enabled: "+s);
+        Toast.makeText(this, "Enabled new provider " + bestProvider,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        System.out.println("Provider disabled: "+s);
+        Toast.makeText(this, "Disabled provider " + bestProvider,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle b) {
+        System.out.println("Status changed: "+s+" "+i);
     }
 
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint({"MissingPermissions", "MissingPermission"})
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        System.out.println("onMapReady(): Map ready.");
+
         mMap = googleMap;
 
+
+        LatLng ttu = new LatLng(33.584468, -101.874658);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ttu, 15));
+
+
         mMap.getUiSettings().setZoomControlsEnabled(true); //Yong 03082018
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+        if(havePermissions()) {
+
+            mMap.setMyLocationEnabled(true);   //Yong 03082018
+            mMap.setOnMyLocationButtonClickListener(this);
+
+
+            mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+                @Override
+                public void onMapLongClick(LatLng latLng) {
+                    {
+                        if (listPoints.size() == 2) {
+                            listPoints.clear();
+                            mMap.clear();
+                        }
+                        listPoints.add(latLng);
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+
+                        if (listPoints.size() == 1) {
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                        } else {
+                            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                        }
+                        mMap.addMarker(markerOptions);
+                        //TODO get direction url
+
+                    }
+                }
+
+            });
         }
 
-        mMap.setMyLocationEnabled(true);   //Yong 03082018
+     }
 
-        mMap.setOnMapLongClickListener( new GoogleMap.OnMapLongClickListener()
-        {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                {
-                    if (listPoints.size() == 2)
-                    {
-                        listPoints.clear();
-                        mMap.clear();
-                    }
-                    listPoints.add(latLng);
-                    MarkerOptions markerOptions= new MarkerOptions();
-                    markerOptions.position(latLng);
-
-                    if  (listPoints.size()==1)
-                    {
-                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                    }
-                    else
-                    {
-                        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                    }
-                    mMap.addMarker(markerOptions);
-                    //TODO get direction url
-
-                }
-            }
-
-        });
-
+    private void doPathToSingleDestination() {
         if(singleDestination != null) {
             System.out.println("Creating oneshot destination");
             LatLng ttu = new LatLng(33.584468, -101.874658);
@@ -137,53 +228,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.addMarker(new MarkerOptions().position(singleDestination).title(singleDestinationName));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ttu, 15));
 
-            //String serverKey = Resources.getString(R.string.google_maps_key); .  findViewById(R.string.google_maps_key).toString();
             final String serverKey = getApplicationContext().getString(R.string.directions_key);
-            //System.out.println(serverKey);
-            LatLng origin = ttu;
-            LatLng destination = singleDestination;
-            GoogleDirection.withServerKey(serverKey)
-                    .from(origin)
-                    .to(destination)
-                    .transportMode(TransportMode.WALKING)
-                    .execute(new DirectionCallback() {
-                        @Override
-                        public void onDirectionSuccess(Direction direction, String rawBody) {
-                            // Do something here
-                            System.out.println("Direction success: "+rawBody);
-                            Route route = direction.getRouteList().get(0);
-                            Leg leg = route.getLegList().get(0);
-                           // List<StepList> sls = leg.getStepList();
-                            ArrayList<LatLng> directionPositionList = leg.getDirectionPoint();
-                            PolylineOptions polylineOptions = DirectionConverter.createPolyline(getApplicationContext(), directionPositionList, 5, Color.RED);
-                            mMap.addPolyline(polylineOptions);
-                        }
+            if(myLocation != null) {
+                LatLng origin = new LatLng(myLocation.getLatitude(), myLocation.getLongitude()); //ttu;
+                LatLng destination = singleDestination;
+                GoogleDirection.withServerKey(serverKey)
+                        .from(origin)
+                        .to(destination)
+                        .transportMode(TransportMode.WALKING)
+                        .execute(new DirectionCallback() {
+                            @Override
+                            public void onDirectionSuccess(Direction direction, String rawBody) {
+                                System.out.println("Direction success: " + rawBody);
+                                Route route = direction.getRouteList().get(0);
+                                Leg leg = route.getLegList().get(0);
+                                ArrayList<LatLng> directionPositionList = leg.getDirectionPoint();
+                                PolylineOptions polylineOptions = DirectionConverter.createPolyline(getApplicationContext(), directionPositionList, 5, Color.RED);
+                                mMap.addPolyline(polylineOptions);
+                            }
 
-                        @Override
-                        public void onDirectionFailure(Throwable t) {
-                            // Do something here
-                            System.out.println(t);
-                        }
-                    });
+                            @Override
+                            public void onDirectionFailure(Throwable t) {
+                                System.out.println(t);
+                            }
+                        });
+            } else {
+                Toast.makeText(this, "No location; aborted directions", 5).show();
+
+            }
         } else {
             System.out.println("Do not have oneshot destination");
         }
-     }
+    }
 
     @SuppressLint({"MissingPermissions", "MissingPermission"})
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case LOCATION_REQUEST:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mMap.setMyLocationEnabled(true);
+    private Location getLastKnownLocation() {
+        System.out.println("getLastKnownLocation(): started");
+
+        if(havePermissions()) {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            List<String> providers = locationManager.getProviders(true);
+            Location bestLocation = null;
+            for (String provider : providers) {
+                Location l = locationManager.getLastKnownLocation(provider);
+                if (l == null) {
+                    System.out.println("getLastKnownLocation(): Location from provider " + provider + ": null");
+                    continue;
                 }
-                break;
+                System.out.println("getLastKnownLocation(): Location from provider " + provider + ": " + l);
+                if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                    // Found best last known location: %s", l);
+                    bestLocation = l;
+                }
+            }
+            return bestLocation;
         }
-        //super.onRequestPermissionsResult ( requestCode, permissions, grantResults );
-        // TTU memorial circle: 33.584468, -101.874658
-        LatLng ttu = new LatLng(33.584468, -101.874658);
-        mMap.addMarker(new MarkerOptions().position(ttu).title("Texas Tech Memorial Circle"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ttu));
+        return null;
     }
+
+    private boolean havePermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("getPermissions(): Permission to GPS denied.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+            return false;
+        }
+        /*else if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("getPermissions(): Permission to coarse location denied.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_REQUEST);
+            return false;
+        }*/
+        return true;
+    }
+
+
+
 }
