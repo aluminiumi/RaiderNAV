9be7d587cef0f9753bfc3817dd0a852919964188
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2018-04-11 20:39:47 -0500

    mapped out unit test methods for sfh and sh

--- app/src/test/java/com/deaftone/tableware/raidernav/ScheduleFileHandlerUnitTest.java
+++ app/src/test/java/com/deaftone/tableware/raidernav/ScheduleFileHandlerUnitTest.java
@@ -1,5 +1,10 @@
 package com.deaftone.tableware.raidernav;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -11,16 +16,28 @@ public class ScheduleFileHandlerUnitTest {
         assertEquals(4, 2 + 2);
     }*/
 
-    /*@Before
+    ScheduleFileHandler sfh;
+
+    @Before
     public void initialize() {
-        AddressMap.initialize();
+        //sfh = new ScheduleFileHandler();
     }
 
     @Test
-    public void getKeys_isCorrect() {
-        assertEquals("ADMIN", AddressMap.getKeys().iterator().next());
+    public void writeFile_isCorrect() {
+        //assertEquals("ADMIN", AddressMap.getKeys().iterator().next());
 
-    }*/
+    }
+
+    @Test
+    public void readFile_isCorrect() {
+
+    }
+
+    @Test
+    public void fileExists_isCorrect() {
+
+    }
 
 
 }
--- app/src/test/java/com/deaftone/tableware/raidernav/ScheduleHandlerUnitTest.java
+++ app/src/test/java/com/deaftone/tableware/raidernav/ScheduleHandlerUnitTest.java
@@ -1,5 +1,7 @@
 package com.deaftone.tableware.raidernav;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,16 +18,47 @@ public class ScheduleHandlerUnitTest {
         assertEquals(4, 2 + 2);
     }*/
 
-    /*@Before
+    ScheduleHandler sh;
+
+    @Before
     public void initialize() {
-        AddressMap.initialize();
+        //sh = new ScheduleHandler(new ApplicationContext());
     }
 
     @Test
-    public void getKeys_isCorrect() {
-        assertEquals("ADMIN", AddressMap.getKeys().iterator().next());
+    public void initializeSchedule_isCorrect() {
+        //assertEquals("ADMIN", AddressMap.getKeys().iterator().next());
 
-    }*/
+    }
 
+    @Test
+    public void getMasterList_isCorrect() {
+
+    }
+
+    @Test
+    public void addScheduleToMasterList_isCorrect() {
+
+    }
+
+    @Test
+    public void removeScheduleFromMasterList_isCorrect() {
+
+    }
+
+    @Test
+    public void replaceScheduleInMasterList_isCorrect() {
+
+    }
+
+    @Test
+    public void saveMasterListToFile_isCorrect() {
+
+    }
+
+    @Test
+    public void loadMasterListFromFile_isCorrect() {
+
+    }
 
 }
